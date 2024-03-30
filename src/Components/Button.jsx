65f400f1fbff434def5cb6e5e154593448ef804b
Rehan-sarwar <rehan232167@gmail.com>
2024-03-30 21:36:45 +0500

    Hoof

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { FaBars } from "react-icons/fa6"
 
-function Button({ classs, text="Button", iocn}) {
+function Button() {
   return (
     <div>
-      <img src="images.png" alt="" />
+      <div className='flex  justify-between  p-[5px] px-5  items-center  '>
+      <img className='w-[200px]' src="Groovy1.png" alt="" />
+     <div className='flex  gap-10 text-[18px] text-white -bottom-full '>
+     <ul class=" navbar-nav flex items-center gap-[40px]  ">
+      <li class="nav-item ">
+      <a class="nav-link" href="about">Dietary</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="heath">Health</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Grooming</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="puppies">Puppies</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="blog">Blogs</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="about">About Us</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    </ul>
+     </div>
+    </div>
+    <div className='flex items-center p-5 justify-between px-10 lg:hidden'>
+    <img className='w-[100px]' src="Groovy1.png" alt="" />
+     <FaBars  /> 
+
+    </div>
     </div>
     
   )
