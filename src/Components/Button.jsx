738252235d152055b18e33d7cc2a369fd8333ccd
Rehan-sarwar <rehan232167@gmail.com>
2024-03-30 21:25:44 +0500

    Button

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button({ classs, text="Button", iocn}) {
+  return (
+    <div>
+      <img src="images.png" alt="" />
+    </div>
+    
+  )
+}
+
+export default Button
